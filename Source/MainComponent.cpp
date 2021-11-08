b06c328d8c13e537db6cd146008aa625ae02c4e9
Giulio Iacomino <giulio.iacomino92@gmail.com>
2021-11-08 16:07:59 +0000

    MidiOutput(YESSSS)

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -10,8 +10,16 @@ MainComponent::MainComponent()
         DBG("Input " << i << ": " << midiInputDevices[i].name << "\n");
     }
     
-    audioDeviceManager.setMidiInputDeviceEnabled(midiInputDevices[1].identifier, true);
+    auto midiOutputDevices = MidiOutput::getAvailableDevices();
+    for (auto i = 0; i < midiOutputDevices.size(); i++)
+    {
+        DBG("Output " << i << ": " << midiOutputDevices[i].name << "\n");
+    }
+    audioDeviceManager.setDefaultMidiOutputDevice (midiOutputDevices[1].identifier);
+    
+    audioDeviceManager.setMidiInputDeviceEnabled(midiInputDevices[2].identifier, true);
     audioDeviceManager.addMidiInputDeviceCallback("", this);
+    
     
     addAndMakeVisible(midiLabel);
     midiLabel.setEditable(true);
@@ -19,7 +27,7 @@ MainComponent::MainComponent()
     midiLabel.setText ("YO", juce::dontSendNotification);
     midiLabel.setColour (juce::Label::textColourId, juce::Colours::lightgreen);
     midiLabel.setJustificationType (juce::Justification::centred);
-    midiLabel.setSize(getWidth(), getHeight());
+    midiLabel.setSize(getWidth()/4, getHeight()/4);
     
 }
 
@@ -46,7 +54,7 @@ void MainComponent::resized()
 
 void MainComponent::handleIncomingMidiMessage(MidiInput *source, const MidiMessage &message)
 {
-    DBG("Message received\n");
+    audioDeviceManager.getDefaultMidiOutput()->sendMessageNow (message);
     String midiText;
     if (message.isNoteOnOrOff())
     {
@@ -55,6 +63,7 @@ void MainComponent::handleIncomingMidiMessage(MidiInput *source, const MidiMessa
         midiText << " Velocity " << message.getVelocity();
     }
     midiLabel.getTextValue() = midiText;
+    
 }
 
 
